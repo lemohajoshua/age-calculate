b64eafe515fa0bca5d459e7f34d8694c3220c282
lemohajoshua <lemohajoshua89@gmail.com>
2025-09-22 22:51:50 +0100

    worked on the validation

--- script.js
+++ script.js
@@ -11,12 +11,54 @@ const dayResult = document.getElementById("day-result");
 
 const submitButton = document.getElementById("submit");
 
+dayInput.addEventListener("input", function () {
+    if (dayInput.value.length > 2) {
+        dayInput.value = dayInput.value.slice(0,2);
+    }
+    if (dayInput.value > 31) dayInput.value = 0;
+});
+
+monthInput.addEventListener("input", function () {
+    if (monthInput.value.length > 2) {
+        monthInput.value = monthInput.value.slice(0,2);
+    }
+    if (monthInput.value > 12) monthInput.value = 0;
+});
+
+yearInput.addEventListener("input", function () {
+    if (yearInput.value.length > 4) {
+        yearInput.value = yearInput.value.slice(0,4);
+    }
+    if (yearInput.value > 9999) yearInput.value = 0
+})
+
 submitButton.addEventListener("click", function () {
-    // e.preventDefault(); 
+    // e.preventDefault();
+    document.getElementById("day-error").textContent = "";
+    document.getElementById("month-error").textContent = "";
+    document.getElementById("year-error").textContent = "";
 
     const dd = Number(dayInput.value);
     const mm = Number(monthInput.value) - 1;
     const yy = Number(yearInput.value);
+
+    let hasError = false;
+
+    if (dd < 1 || dd > 31) {
+        document.getElementById("day-error").textContent = " wrong day";
+        hasError = true;
+    }
+    if (mm < 0 || mm > 11) {
+        document.getElementById("month-error").textContent = "wrong month";
+        hasError = true;
+    }
+    if (yearInput.value.length !== 4) {
+        document.getElementById("year-error").textContent = "wrong year";
+        hasError = true;
+    }
+
+    if (hasError) return;
+
 
     if (!dd || !monthInput.value || !yy) {
         alert("Please enter your date");
